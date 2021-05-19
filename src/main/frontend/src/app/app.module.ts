b3d70c30659e66aa56501866c2d0880731925e04
jcarbad <jcarbad@gmail.com>
2021-05-18 22:15:21 -0600

    Added Angular app with Bootstrap

---
+++ src/main/frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TasksAddComponent } from './tasks/tasks-add/tasks-add.component';
+import { TasksListComponent } from './tasks/tasks-list/tasks-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TasksComponent,
+    TasksAddComponent,
+    TasksListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
